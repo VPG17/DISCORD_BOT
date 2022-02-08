d7b711c1fc2d1955ab5e375292ed733190a08b36
Vicente Pedro García De La Morena <vgarciamorena@cifpfbmoll.eu>
2022-02-08 09:58:58 +0100

    CASE INSENSITIVE MESSAGE

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if message.content.startswith('hola'):
+
+    string1 = message.content;
+
+    if string1.lower().startswith('hello'):
         await message.channel.send('Hello!')
 
 client.run(os.getenv('TOKEN'))
